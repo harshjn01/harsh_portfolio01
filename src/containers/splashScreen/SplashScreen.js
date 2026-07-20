88e58a3005617db1b7a91828955c4ee2b9d0736d
Harsh Jain <harshjain6032@gmail.com>
2026-07-20 15:29:26 +0530

    Replace Lottie splash screen with dynamic text splash screen

--- src/containers/splashScreen/SplashScreen.js
+++ src/containers/splashScreen/SplashScreen.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, {useContext} from "react";
+import "./SplashScreen.css";
 import "./SplashScreen.scss";
-import DisplayLottie from "../../components/displayLottie/DisplayLottie";
-import {splashScreen} from "../../portfolio";
+import {greeting} from "../../portfolio";
+import StyleContext from "../../contexts/StyleContext";
 
 export default function SplashScreen() {
+  const {isDark} = useContext(StyleContext);
   return (
-    <div className="splash-screen">
-      <DisplayLottie animationData={splashScreen.animation} />
+    <div className={isDark ? "splash-container dark-mode" : "splash-container"}>
+      <div className="splash-title-container">
+        <span style={{color: "#868e96"}}> &lt;</span>
+        <span className="splash-title" style={{color: isDark ? "white" : "black"}}>{greeting.username}</span>
+        <span style={{color: "#868e96"}}>/&gt;</span>
+      </div>
     </div>
   );
 }

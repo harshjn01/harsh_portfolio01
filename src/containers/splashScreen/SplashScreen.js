f71aca6d0248ae0be5dd17709bb7ecc7de3cbd69
Harsh Jain <harshjain6032@gmail.com>
2026-07-20 15:34:40 +0530

    Restore cube animation to splash screen

--- src/containers/splashScreen/SplashScreen.js
+++ src/containers/splashScreen/SplashScreen.js
@@ -1,13 +1,17 @@
 import React, {useContext} from "react";
 import "./SplashScreen.css";
 import "./SplashScreen.scss";
-import {greeting} from "../../portfolio";
+import DisplayLottie from "../../components/displayLottie/DisplayLottie";
+import {splashScreen, greeting} from "../../portfolio";
 import StyleContext from "../../contexts/StyleContext";
 
 export default function SplashScreen() {
   const {isDark} = useContext(StyleContext);
   return (
     <div className={isDark ? "splash-container dark-mode" : "splash-container"}>
+      <div className="splash-animation-container">
+        <DisplayLottie animationData={splashScreen.animation} />
+      </div>
       <div className="splash-title-container">
         <span style={{color: "#868e96"}}> &lt;</span>
         <span className="splash-title" style={{color: isDark ? "white" : "black"}}>{greeting.username}</span>
